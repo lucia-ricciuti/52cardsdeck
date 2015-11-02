94630c6f6b3826343f1e7384b61631b77803f599
Lucia Ricciuti <lucia.ricciuti@gmail.com>
2015-11-02 00:11:28 -0600

    Added exception handling in main

--- src/main/java/it/luciaricciuti/ftcardsdeck/App.java
+++ src/main/java/it/luciaricciuti/ftcardsdeck/App.java
@@ -26,57 +26,63 @@ public class App
     	
     	logger.info("App starting ....");
     	
-    	// Creating displayers
-    	DeckDisplayer deckDisplayer = new DeckDisplayer();
-    	CardDisplayer cardDisplayer = new CardDisplayer();
+    	try {
+        	// Creating displayers
+        	DeckDisplayer deckDisplayer = new DeckDisplayer();
+        	CardDisplayer cardDisplayer = new CardDisplayer();
 
-    	// Creating a deck
-    	Deck deck = new Deck();
-    	
-    	// Showing deck before operations
-    	logger.info("A new deck is ready. \n" + deckDisplayer.display(deck));
-    	
-    	// Instantiating a manager
-    	DeckManager manager = new StraightDeckManager();
-    	
-    	// 1. Shuffling
-    	logger.info("Shuffling the deck ...");
-    	manager.shuffle(deck);
-    	
-    	// Showing deck after shuffling
-    	logger.info("Here is the shuffled deck. \n" + deckDisplayer.display(deck));
+        	// Creating a deck
+        	Deck deck = new Deck();
+        	
+        	// Showing deck before operations
+        	logger.info("A new deck is ready. \n" + deckDisplayer.display(deck));
+        	
+        	// Instantiating a manager
+        	DeckManager manager = new StraightDeckManager();
+        	
+        	// 1. Shuffling
+        	logger.info("Shuffling the deck ...");
+        	manager.shuffle(deck);
+        	
+        	// Showing deck after shuffling
+        	logger.info("Here is the shuffled deck. \n" + deckDisplayer.display(deck));
 
-    	// 2. Get all cards with the same suit from a shuffled deck
-    	Suit selectedSuit = Suit.HEARTS;
-    	logger.info("Getting all cards with "+selectedSuit+" ...");
-    	List<Card> cardsWithHearts = manager.extractCards(deck, selectedSuit);
-    	
-    	// Showing extracted cards
-    	logger.info("Here are the extracted cards:\n");
-    	cardsWithHearts.forEach(card -> logger.info(cardDisplayer.display(card)));
-    	
-    	// 3. Ordering the deck
-    	logger.info("Ordering the deck ...");
-    	manager.order(deck);
-    	
-    	// Showing the ordered deck
-    	logger.info("Here is the ordered deck. \n" + deckDisplayer.display(deck));
+        	// 2. Get all cards with the same suit from a shuffled deck
+        	Suit selectedSuit = Suit.HEARTS;
+        	logger.info("Getting all cards with "+selectedSuit+" ...");
+        	List<Card> cardsWithHearts = manager.extractCards(deck, selectedSuit);
+        	
+        	// Showing extracted cards
+        	logger.info("Here are the extracted cards:\n");
+        	cardsWithHearts.forEach(card -> logger.info(cardDisplayer.display(card)));
+        	
+        	// 3. Ordering the deck
+        	logger.info("Ordering the deck ...");
+        	manager.order(deck);
+        	
+        	// Showing the ordered deck
+        	logger.info("Here is the ordered deck. \n" + deckDisplayer.display(deck));
 
-    	// Instantiating a custom manager
-    	CustomDeckManager customManager = new CustomDeckManager();
-    	
-    	// Shuffling again the deck
-    	logger.info("Shuffling deck again ....");
-    	customManager.shuffle(deck);
-    	logger.info("Here is the newly shuffled deck. \n" + deckDisplayer.display(deck));
+        	// Instantiating a custom manager
+        	CustomDeckManager customManager = new CustomDeckManager();
+        	
+        	// Shuffling again the deck
+        	logger.info("Shuffling deck again ....");
+        	customManager.shuffle(deck);
+        	logger.info("Here is the newly shuffled deck. \n" + deckDisplayer.display(deck));
 
-    	// Ordering deck with custom manager
-    	logger.info("Ordering the deck with custom manager ...");
-    	customManager.order(deck);
-    	
-    	// Showing the ordered deck
-    	logger.info("Here is the ordered deck. \n" + deckDisplayer.display(deck));
+        	// Ordering deck with custom manager
+        	logger.info("Ordering the deck with custom manager ...");
+        	customManager.order(deck);
+        	
+        	// Showing the ordered deck
+        	logger.info("Here is the ordered deck. \n" + deckDisplayer.display(deck));
 
-    	logger.info("App finished ....");
+        	logger.info("App finished ....");
+		} 
+    	catch (Exception e) {
+    		logger.error("An unexpected error occurred", e);
+		}
+    	
     }
 }
